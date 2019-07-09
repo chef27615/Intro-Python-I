bc5d45796a4c296d9c137438a7353c372d0e2a12
Wei Du <wei.du.2015@marshall.usc.edu>
2019-07-09 17:35:24 -0400

    txt file written and read as intented, finished file 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt', 'r') as f:
+    for line in f:
+        print(line)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bars = ['bar1', 'bar2', 'bar3']
+
+with open('bar.txt', 'w') as f:
+    for bar in bars:
+        print(bar, file=f)
