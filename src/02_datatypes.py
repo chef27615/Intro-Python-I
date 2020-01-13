0b1b886787250b4097d2634f3cac547c83f2f6d3
chef27615 <wei.du.2015@marshall.usc.edu>
2020-01-13 13:20:27 -0500

    00-02

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,11 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+a = int(y)
+print(x+a)
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+b = str(x)
+print(b+y)
