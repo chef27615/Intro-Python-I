6977b2f3b4e943d1f726ef65c91c57a476d8279b
chef27615 <wei.du.2015@marshall.usc.edu>
2020-01-14 18:25:39 -0500

    resubmit 8

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,18 +10,14 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-for x in range(6):
-    y.append(x)
+y = [i for i in range(6)]
+
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-
-for num in range (10):
-    y.append(num*num*num)
+y = [num**3 for num in range(10)]
 
 print(y)
 
@@ -29,9 +25,7 @@ print(y)
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
-y = []
-for st in a:
-    y.append(st.upper())
+y = [word.upper() for word in a]
 
 print(y)
 
@@ -41,8 +35,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
-for el in x:
-    if x.index(el)%2==0:
-        y.append(el)
+y = [el for el in x if x.index(el)%2==0]
+
 print(y)
